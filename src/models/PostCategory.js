eb31b95fd5567e3df23557268bf9b16896f9b872
Juliana Ando <juandocezare@gmail.com>
2023-06-18 13:05:40 -0300

    Req 11 done

---
+++ src/models/PostCategory.js
@@ -0,0 +1,31 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'PostsCategories',
+  },
+);
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+}
+
+module.exports = PostCategory;
